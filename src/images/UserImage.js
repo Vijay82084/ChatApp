f3c429a6c6fd5c231014c587416f6e9ebeb4ffc3
Vijay Ramcharan Lodhirajput <144595529+Vijay82084@users.noreply.github.com>
2024-03-06 13:04:35 +0530

    Add files via upload

---
+++ src/images/UserImage.js
@@ -0,0 +1,6 @@
+     export default function UserImage(){
+      const   Image={  
+        data:12,
+    image:"https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=",
+}
+     }
---
+++ src/reducers/user.js
@@ -0,0 +1,14 @@
+
+
+export default function user(state = {}) {
+ 
+   
+    return {
+      id: 1,
+      name: "Vijay Lodhirajput",
+      image: "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=",
+   
+  
+    }
+  
+  }
